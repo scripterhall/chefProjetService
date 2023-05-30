9b3544726dbdfa23b2e566b5dd2163603480e5da
nourallah <halloul.nourallah@gmail.com>
2023-05-30 04:16:30 +0100

    put

--- src/main/java/com/ms/gestionchefProjetservice/controller/ChefProjetController.java
+++ src/main/java/com/ms/gestionchefProjetservice/controller/ChefProjetController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -60,6 +61,12 @@ public class ChefProjetController {
     public ChefProjet ajoutChefProjet(@RequestBody ChefProjet chp){
         if(chp==null)
             return null;
+        return this.chefProjetService.ajouterChefProjet(chp);
+    }
+
+
+    @PutMapping
+    public ChefProjet modifierChefProjet(@RequestBody ChefProjet chp){
         return this.chefProjetService.ajouterChefProjet(chp);
     }
 
